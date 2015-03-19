629da8fd60e6eb17bda6d992aaf3c85f8883f45c
randvoorhies <rand.voorhies@gmail.com>
2015-03-19 02:02:22 +0000

    Auto-updating Doxygen developer documentation

--- search/all_63.js
+++ search/all_63.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['create',['create',['../structrcv_1_1cubehelix_1_1create.html',1,'rcv::cubehelix']]],
-  ['cubehelix',['cubehelix',['../classrcv_1_1cubehelix.html#a3e972d476fe27ca00ee0da8bd99a5ce8',1,'rcv::cubehelix']]],
-  ['cubehelix',['cubehelix',['../classrcv_1_1cubehelix.html',1,'rcv']]]
+  ['cubehelix',['cubehelix',['../classrcv_1_1cubehelix.html',1,'rcv']]],
+  ['cubehelix',['cubehelix',['../classrcv_1_1cubehelix.html#a3e972d476fe27ca00ee0da8bd99a5ce8',1,'rcv::cubehelix']]]
 ];
--- search/classes_63.js
+++ search/classes_63.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['create',['create',['../structrcv_1_1cubehelix_1_1create.html',1,'rcv::cubehelix']]],
   ['cubehelix',['cubehelix',['../classrcv_1_1cubehelix.html',1,'rcv']]]
 ];
